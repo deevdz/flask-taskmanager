3a7809bc52d098edabdc17051ac6156fd0f06d3b
Deirdre van der Zee <deirdrevanderzee@gmail.com>
2019-04-10 09:58:41 +0000

    Add Tasks

--- app.py
+++ app.py
@@ -11,9 +11,12 @@ mongo = PyMongo(app)
 
 @app.route('/')
 @app.route('/get_tasks')
-
 def get_tasks():
    return render_template("tasks.html", tasks=mongo.db.tasks.find())
+   
+@app.route('/add_task')
+def add_task():
+    return render_template('addtask.html')   
 
 if __name__ == '__main__':
     app.run(host=os.environ.get('IP'),
